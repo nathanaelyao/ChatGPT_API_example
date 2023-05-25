82957021d8b0b259f88d35ccd6f0a9d91de49355
Nathanael Yao <60585418+nathanaelyao@users.noreply.github.com>
2023-05-24 20:53:41 -0400

    Update py.py
    fixed parsing

--- docugen/parse/py.py
+++ docugen/parse/py.py
@@ -8,23 +8,28 @@ def find_toplevel_funcs(lines):
 
     for line in lines:
         curr_indent_level = len(line) - len(line.lstrip())
-        function_found = re.search('^def(\s+)(.*)\(.*\)(\s*):(\s*)$', line.strip())
+        function_found = re.search('^def(\s+)(.*)\(.*\)(\s*):(\s*)$', line.strip(), re.MULTILINE)
 
-        if len(function_head.strip()) > 0 and (curr_indent_level < function_body_indent_level):
+        if len(function_head.strip()) > 0 and (curr_indent_level <= function_body_indent_level):
             functions.append({
-                "head": function_head,
+                "head": function_head.strip(),
                 "content": function_content
             })
-            if function_found:
-                function_head = ""
-                function_content = ""
+            function_head = ""
+            function_content = ""
 
         if function_found and len(function_head) == 0:
-            function_head = re.search("(\s+)(.*)\(.*\)", line).string.replace('def', '').replace('):', ')').strip()
+            function_head = re.search("^def(\s+)(.*)\(.*\)(\s*):(\s*)$", line.strip()).group(2).strip()
             function_content = line
             function_body_indent_level = curr_indent_level + 1
 
         elif not function_found and len(function_head.strip()) > 0 and curr_indent_level >= function_body_indent_level:
             function_content += line
 
-    return functions 
+    if len(function_head.strip()) > 0:
+        functions.append({
+            "head": function_head.strip(),
+            "content": function_content
+        })
+
+    return functions
